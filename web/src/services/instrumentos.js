21891ccf27048fa2eff35926e0e70cc3c1fd343c
joelmaykonTADS <joelmaykon94@gmail.com>
2020-11-06 22:51:02 -0300

    retonando a response

--- web/src/services/instrumentos.js
+++ web/src/services/instrumentos.js
@@ -4,9 +4,7 @@ const url = process.env.VUE_APP_URL_API;
 
 async function salvar(instrumento) {
   await http.post(`${url}/instrumentos`, instrumento).then((response) => {
-    return response.status == 200 ? true : false;
+    return response; 
   });
 }
-export {
-  salvar
-};
+export { salvar };
