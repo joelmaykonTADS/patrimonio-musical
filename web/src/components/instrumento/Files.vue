a4495edd4a0ed785688adca11e035e1eb5733630
joelmaykon <joelmaykon94@gmail.com>
2020-11-30 08:21:56 -0300

    alinhamento dos campos e anexos do instrumento

---
+++ web/src/components/instrumento/Files.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-container>
+    <v-hover>
+      <v-card
+        slot-scope="{ hover }"
+        :class="`elevation-${hover ? 12 : 2}`"
+        class="mx-auto"
+      >
+        <v-card-text>
+          <file-pond
+            name="test"
+            ref="pond"
+            class="mouse-open"
+            class-name="my-pond"
+            :label-idle="label"
+            allow-multiple="true"
+            accepted-file-types="application/pdf"
+            v-bind:files="myFiles"
+            v-on:init="handleFilePondInit"
+          />
+        </v-card-text>
+      </v-card>
+    </v-hover>
+  </v-container>
+</template>
+
+<script>
+// Import FilePond
+import vueFilePond from "vue-filepond";
+// Import plugins
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type/dist/filepond-plugin-file-validate-type.esm.js";
+import FilePondPluginFilePoster from "filepond-plugin-file-poster";
+
+// Import styles
+import "filepond/dist/filepond.min.css";
+import "filepond-plugin-file-poster/dist/filepond-plugin-file-poster.css";
+// Create FilePond component
+const FilePond = vueFilePond(
+  FilePondPluginFileValidateType,
+  FilePondPluginFilePoster
+);
+export default {
+  props:{label:String},
+  components: {
+    FilePond,
+  },
+  data() {
+    return {
+      myFiles: [],
+    };
+  },
+};
+</script>
+
+<style>
+.mouse-open {
+  cursor: pointer;
+}
+
+/* the color of the focus ring */
+.filepond--file-action-button:hover,
+.filepond--file-action-button:focus {
+  box-shadow: 0 0 0 0.125em rgba(255, 255, 255, 0.9);
+}
+
+.filepond--root {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
+    Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+}
+
+.filepond--item {
+  width: calc(50% - 0.5em);
+}
+</style>
--- web/src/components/instrumento/Instrumento.vue
+++ web/src/components/instrumento/Instrumento.vue
@@ -1,172 +1,222 @@
 <template>
   <v-container fluid>
-    <v-row class="d-flex justify-center">
-      <v-col cols="3">
-        <v-combobox
-          v-model="nome"
-          :items="nomes"
-          color="grey"
-          item-color="grey darken-2"
-          label="Nome do instrumento"
-          placeholder="Informe o nome"
-          outlined
-        />
-      </v-col>
-      <v-col cols="3">
-        <v-combobox
-          v-model="caracteristica"
-          :items="caracteristicas"
-          color="grey"
-          label="Característica"
-          placeholder="Característica ou tonalidade"
-          outlined
-        />
-      </v-col>
-      <v-col cols="3">
-        <v-combobox
-          v-model="tombamento"
-          :items="tombamentos"
-          color="grey"
-          label="Tombamento"
-          placeholder="Informe o número"
-          outlined
-        ></v-combobox>
-      </v-col>
-    </v-row>
-    <v-row class="d-flex justify-center">
-      <v-col cols="3">
-        <v-combobox
-          v-model="ano"
-          :items="anos"
-          color="grey"
-          label="Ano"
-          placeholder="Ano de fabricação"
-          outlined
-        />
-      </v-col>
-      <v-col cols="3">
-        <v-combobox
-          v-model="marca"
-          :items="marcas"
-          color="grey"
-          label="Marca"
-          placeholder="Marca do instrumento"
-          outlined
-        ></v-combobox>
-      </v-col>
-      <v-col cols="3">
-        <v-combobox
-          v-model="componentes"
-          color="grey"
-          label="Componentes"
-          placeholder="Componentes do instrumento"
-          outlined
-          multiple
-        ></v-combobox>
-      </v-col>
-    </v-row>
-    <v-row class="d-flex justify-center">
+    <v-row>
       <v-col cols="9">
-        <v-textarea
-          v-model="observacoes"
-          color="grey"
-          label="Observações"
-          rows="2"
-          placeholder="Descrição da situação do instrumento"
-          outlined
-        ></v-textarea>
+        <v-row class="d-flex">
+          <v-col cols="4">
+            <v-combobox
+              v-model="nome"
+              :items="nomes"
+              color="grey"
+              item-color="grey darken-2"
+              label="Nome do instrumento"
+              placeholder="Informe o nome"
+              outlined
+            />
+          </v-col>
+          <v-col cols="4">
+            <v-combobox
+              v-model="caracteristica"
+              :items="caracteristicas"
+              color="grey"
+              label="Característica"
+              placeholder="Característica ou tonalidade"
+              outlined
+            />
+          </v-col>
+          <v-col cols="4">
+            <v-combobox
+              v-model="tombamento"
+              :items="tombamentos"
+              color="grey"
+              maxlength="4"
+              label="Tombamento"
+              placeholder="Informe o número"
+              outlined
+            ></v-combobox>
+          </v-col>
+        </v-row>
+        <v-row class="d-flex justify-center form-row-top">
+          <v-col cols="4">
+            <v-combobox
+              v-model="ano"
+              :items="anos"
+              maxlength="4"
+              color="grey"
+              label="Ano"
+              placeholder="Ano de fabricação"
+              outlined
+            />
+          </v-col>
+          <v-col cols="4">
+            <v-combobox
+              v-model="marca"
+              :items="marcas"
+              color="grey"
+              label="Marca"
+              placeholder="Marca do instrumento"
+              outlined
+            ></v-combobox>
+          </v-col>
+          <v-col cols="4">
+            <v-combobox
+              v-model="componentes"
+              color="grey"
+              label="Componentes"
+              placeholder="Componentes do instrumento"
+              outlined
+              multiple
+            ></v-combobox>
+          </v-col>
+        </v-row>
+        <v-row class="d-flex form-row-top">
+          <v-col cols="12">
+            <v-textarea
+              v-model="observacoes"
+              color="grey"
+              label="Observações"
+              rows="2"
+              placeholder="Descrição da situação do instrumento"
+              outlined
+            ></v-textarea>
+          </v-col>
+        </v-row>
+        <v-row class="d-flex justify-center form-row-top">
+          <v-col cols="4">
+            <v-combobox
+              v-model="empresa"
+              :items="empresas"
+              color="grey"
+              label="Empresa"
+              placeholder="Empresa onde comprou"
+              outlined
+            ></v-combobox>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="notaFiscal"
+              color="grey"
+              label="Nota fiscal da compra ou doação"
+              placeholder="Número da nota fiscal"
+              outlined
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="valor"
+              color="grey"
+              label="Valor"
+              placeholder="Valor da compra"
+              outlined
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row class="d-flex justify-center form-row-top">
+          <v-col cols="4">
+            <v-text-field
+              v-model="data"
+              color="grey"
+              label="Data da compra ou doação"
+              placeholder="Data da compra ou doação"
+              outlined
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-combobox
+              v-model="origem"
+              :items="origens"
+              color="grey"
+              label="Origem da doação"
+              placeholder="Origem da doação"
+              outlined
+            ></v-combobox>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="observacoesDoacao"
+              color="grey"
+              label="Observações"
+              placeholder="Descrição sobre a doação"
+              outlined
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="3">
+        <v-row class="text-center">
+          <v-col>
+            <span class="title font-weight-bold">Anexos do instrumento</span>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <files label="Nota fiscal" />  
+            <files label="Termo da doação" />  
+             <files label="Outros documentos" />            
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
-    <v-row class="d-flex justify-center">
-      <v-col cols="3">
-        <v-combobox
-          v-model="empresa"
-          :items="empresas"
-          color="grey"
-          label="Empresa"
-          placeholder="Empresa onde comprou"
-          outlined
-        ></v-combobox>
-      </v-col>
-      <v-col cols="3">
-        <v-text-field
-          v-model="notaFiscal"
-          color="grey"
-          label="Nota fiscal da compra ou doação"
-          placeholder="Número da nota fiscal"
-          outlined
-        ></v-text-field>
-      </v-col>
-      <v-col cols="3">
-        <v-text-field
-          v-model="valor"
-          color="grey"
-          label="Valor"
-          placeholder="Valor da compra"
-          outlined
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row class="d-flex justify-center">
-      <v-col cols="3">
-        <v-text-field
-          v-model="data"
-          color="grey"
-          label="Data da compra ou doação"
-          placeholder="Data da compra ou doação"
-          outlined
-        ></v-text-field>
-      </v-col>
-      <v-col cols="3">
-        <v-combobox
-          v-model="origem"
-          :items="origens"
-          color="grey"
-          label="Origem da doação"
-          placeholder="Origem da doação"
-          outlined
-        ></v-combobox>
-      </v-col>
-      <v-col cols="3">
-        <v-text-field
-          v-model="observacoesDoacao"
-          color="grey"
-          label="Observações"
-          placeholder="Descrição sobre a doação"
-          outlined
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row class="d-flex justify-end">
-      <v-col cols="2">
-        <v-btn color="grey darken-1" class="white--text" @click="BuscarNomes()"
-          >Cancelar</v-btn
-        >
-      </v-col>
-      <v-col cols="3">
+    <v-row class="d-flex justify-end py-auto">
+      <v-speed-dial
+        bottom
+        absolute
+        right
+        direction="top"
+        open-on-hover
+        transition="slide-y-reverse-transition"
+      >
+        <template v-slot:activator>
+          <v-btn color="grey darken-4" dark fab>
+            <v-icon> mdi-alert-circle-outline </v-icon>
+          </v-btn>
+        </template>
         <v-btn
-          v-if="type === 'edit'"
-          color="grey darken-3"
-          @click="editStudents()"
-          class="white--text"
-          >Editar</v-btn
+          fab
+          dark
+          small
+          color="red"
+          @click="voltar()"
+          v-if="type !== 'edit'"
         >
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
         <v-btn
           id="salvar"
           v-if="type !== 'edit'"
-          color="grey darken-3"
+          fab
+          dark
+          small
+          color="green"
           @click="cadastrarInstrumento()"
-          class="white--text"
-          >Salvar</v-btn
         >
-      </v-col>
+          <v-icon>mdi-check</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          dark
+          small
+          color="indigo"
+          v-if="type === 'edit'"
+          @click="editStudents()"
+        >
+          <v-icon>mdi-pencil</v-icon>
+        </v-btn>
+        <v-btn fab dark small color="red" v-if="type === 'edit'">
+          <v-icon>mdi-delete</v-icon>
+        </v-btn>
+      </v-speed-dial>
     </v-row>
   </v-container>
 </template>
 <script>
 import { get, post } from "@/services/repository";
+import Files from "@/components/instrumento/Files";
 
 export default {
+  components: {
+    Files,
+  },
   props: { instrumento: Object, type: String, readonly: Boolean },
   data() {
     return {
@@ -223,6 +273,12 @@ export default {
     this.alterarInstrumento(this.instrumento);
   },
   methods: {
+    handleFilePondInit: function () {
+      console.log("FilePond has initialized");
+
+      // example of instance method call on pond reference
+      this.$refs.pond.getFiles();
+    },
     capitalizeFirstLetter: (str) => {
       return str.charAt(0).toUpperCase() + str.slice(1);
     },
@@ -293,3 +349,17 @@ export default {
   },
 };
 </script>
+<style>
+/* This is for documentation purposes and will not be needed in your application */
+#create .v-speed-dial {
+  position: absolute;
+}
+
+#create .v-btn--floating {
+  position: relative;
+}
+
+.form-row-top {
+  margin-top: -5px;
+}
+</style>
